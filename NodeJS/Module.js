51ab950b2c109706a80f3fd67d5c94767d47a078
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-09-18 01:13:13 +0500

    Modules Added

---
+++ NodeJS/Module.js
@@ -0,0 +1,19 @@
+function Add(a, b) {
+    return a+b;
+}
+function Sub(a, b) {
+    return a-b;
+}
+function Mul(a, b) {
+    return a*b;
+}
+function Div(a, b) {
+    return a/b;
+}
+
+module.exports = {Add, Sub, Mul, Div};
+
+// exports.add = (a, b) => a+b;
+// exports.sub = (a, b) => a+b;
+// exports.mul = (a, b) => a+b;
+// exports.div = (a, b) => a+b;
