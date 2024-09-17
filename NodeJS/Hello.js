027c0058a41e0ad8b4c0adb075589ade345dfbda
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-09-18 01:13:24 +0500

    Print Modules

--- NodeJS/Hello.js
+++ NodeJS/Hello.js
@@ -1,1 +1,7 @@
+const arithmeticOperations = require('./Module');
 console.log("Hello World! From NodeJS Directory");
+
+console.log("5+5: ", arithmeticOperations.Add(5,5));
+console.log("5-5: ", arithmeticOperations.Sub(5,5));
+console.log("5*5: ", arithmeticOperations.Mul(5,5));
+console.log("5/5: ", arithmeticOperations.Div(5,5));
