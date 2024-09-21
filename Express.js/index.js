bc497388a1076ea1705a065620166ad47c37c942
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-09-21 21:35:28 +0500

    Express Startup File

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,1 +1,15 @@
 console.log("Hello World! From Express.js Directory");
+
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our home page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Here is our short history');
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
